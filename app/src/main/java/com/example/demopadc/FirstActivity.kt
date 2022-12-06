936ecc68c152be26e152707a6441d92dd1966eb3
Lordilism <shadowingkoko@gmail.com>
2022-12-06 23:35:08 +0630

    Explicit Intent demo

--- app/src/main/java/com/example/demopadc/FirstActivity.kt
+++ app/src/main/java/com/example/demopadc/FirstActivity.kt
@@ -1,11 +1,47 @@
 package com.example.demopadc
 
+import android.content.Intent
 import android.os.Bundle
+
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        btnclickme.setOnClickListener {
+
+            Toast.makeText(this,"Toast Invoked",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this,"onStart invoked",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this,"onResume invoked",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this,"onPause invoked",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this,"onStop invoked",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this,"onDestroy invoked", Toast.LENGTH_SHORT).show()
     }
 }
